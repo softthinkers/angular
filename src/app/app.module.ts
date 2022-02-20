871ea4bdc8edf148756ee00905ea5002c1126f37
softthinkers <akeelpmajk@gmail.com>
2022-02-20 10:41:04 +0100

    First frontend folder updated wit component card wroking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
